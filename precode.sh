3f5a6e8d79e5ce58e9411f78279a3ee17a1c58e0
Konstantin Kashevarov <kashevarovkb@gmail.com>
2024-09-06 13:37:19 +0300

    popravil

--- precode.sh
+++ precode.sh
@@ -6,20 +6,19 @@
 #   dir2
 #   dir3
 #       dir4
-mkdir -p task/dir3/dir4
+mkdir -p task/dir1 task/dir2 task/dir3/dir4
 # изменяем текущую директорию на task
 cd task
 # создаём пустой файл task/dir2/empty
-touch task/dir2/empty
+touch dir2/empty
 # создаём файл task/dir2/hello.sh с таким содержанием:
-mkdir -p task/dir2
 # #!/bin/bash
 echo "!/bin/bash" > task/dir2/hello.sh
 # echo "$1, привет!"
 echo "$1, привет!" >> task/dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
-chmod 764 /path/to/task/dir
+chmod 764 dir2/hello.sh
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
 ls /path/to/task/dir2 > /path/to/task/dir2/list.txt
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
@@ -35,7 +34,7 @@ export NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-(export NAME="Всем студентам"; ./script/dir2/hello.sh $NAME) >> task/dir1/summary.txt
+bash dir2/hello.sh "$NAME" >> dir1/summary.txt
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 mv task/dir1/summary.txt task/"Практическое задание"
 # выводим на консоль содержимое файла task/Практическое задание
